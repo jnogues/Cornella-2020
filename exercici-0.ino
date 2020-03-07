a3c568953956e20d435944bed7c0902dc627b777
Jaume Nogues <jnogues@gmail.com>
2020-03-07 10:12:55 +0100

    Create exercici-0.ino

---
+++ exercici-0.ino
@@ -0,0 +1,28 @@
+//curs Cornellà 2020
+//Exercici 0
+//06.03.2020
+//nodeMCU
+//@rPrimTech Jaume Nogués
+
+//---------------------------------------------------------------------------------------------
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+char auth[] = "aaaabbbbbcccccddddd"; 
+
+char ssid[] = "WLAN_2020";
+char pass[] = "12345678";
+//---------------------------------------------------------------------------------------------
+
+void setup()
+{
+Serial.begin(115200);
+Serial.println(" ");
+Serial.println("Comencem!");
+Blynk.begin(auth, ssid, pass, IPAddress(xx,xx,xx,xx), 8080);
+}
+
+void loop()
+{
+Blynk.run();
+}
